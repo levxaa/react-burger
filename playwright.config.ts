aae058a8d01aabd38856542481c2e39af0743665
levchenko_a_a <levchenko_a_a@goznak.ru>
2026-05-22 11:24:42 +0300

    no need auth coz har files exist

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,17 +34,17 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
 projects: [
-    {
-      name: 'setup',
-      testMatch: /.*\.setup\.ts/,
-    },
+    // {
+    //   name: 'setup',
+    //   testMatch: /.*\.setup\.ts/,
+    // },
     {
       name: 'chromium',
       use: { 
         ...devices['Desktop Chrome'],
         storageState: 'e2e/.auth/user.json',
       },
-      dependencies: ['setup'],
+     // dependencies: ['setup'],
     },
   ],
 
